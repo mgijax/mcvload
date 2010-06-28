7c1e3ab061f870b015b6e9f9699f3243e4776408
Sharon Giannatto <Sharon.Giannatto@jax.org>
2010-06-28 18:36:55 +0000

    mcvload-4-3-4-8

---
+++ bin/addColumns.py
@@ -0,0 +1,128 @@
+#!/usr/local/bin/python
+#
+#  addColumns.py
+###########################################################################
+#
+#  Purpose:
+#
+#       This script adds columns 9 and 10 to a file
+#
+#  Usage:
+#
+#      checkColumns.py  filename
+#
+#      where:
+#          filename = path to the input file
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#      2:  Discrepancy errors detected in the input files
+#  Implementation:
+#
+#  Notes:  None
+#
+###########################################################################
+
+import string
+import sys
+import os
+
+USAGE = 'Usage: addColumns.py  inputFile'
+TAB = '\t'
+CRT = '\n'
+
+inputFile = None
+fpInput = None
+fpOutput = None
+outputFile = None
+numColumns = os.environ['MCVLOAD_FILE_COLUMNS']
+header = None
+lineList = []
+hasMissingColumns = 0
+
+#
+# Purpose: Validate the arguments to the script.
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Sets global variables.
+# Throws: Nothing
+#
+def checkArgs ():
+    global inputFile
+    if len(sys.argv) != 2:
+        print USAGE
+        sys.exit(1)
+
+    inputFile = sys.argv[1]
+    return
+
+
+#
+# Purpose: Open the file for reading
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Sets global variables.
+# Throws: Nothing
+#
+def openFile ():
+    global fpInput
+    try:
+        fpInput = open(inputFile, 'r')
+    except:
+        print 'Cannot open input file: ' + inputFile
+        sys.exit(1)
+    return
+
+
+#
+# Purpose: Add columns 9 and/or 10 when missing
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Sets global variables.
+# Throws: Nothing
+#
+def addColumns ():
+    global header, lineList, hasMissingColumns
+
+    header = fpInput.readline()
+    lineList.append(header)
+    for line in fpInput.readlines():
+        tokens = string.split(line[:-1], '\t')
+        numCols = len(tokens)
+        print 'numCols: %s' % numCols
+	print 'tokens: %s' %  tokens
+        # if < 8 curator will have to fix, if > 10 ok
+	# if missing 9 or 10 just add it
+        if numCols == 9:
+            line  = '%s%s%s' % (line[:-1], TAB, CRT)
+	    print 'new line: %s' % line
+	    hasMissingColumns = 1
+        elif numCols == 8:
+            line = '%s%s%s%s' % (line[:-1], TAB, TAB, CRT)
+	    print 'new line: %s' % line
+	    hasMissingColumns = 1
+	lineList.append(line)
+
+def writeFile():
+    global fpOutput, outputFile
+    outputFile = '%s.%s' % (inputFile, os.environ['ADD_COLUMNS_EXT'])
+    fpOutput = open(outputFile, 'w')
+    for line in lineList:
+	fpOutput.write(line)
+
+
+#
+# Main
+#
+checkArgs()
+openFile()
+addColumns()
+if hasMissingColumns == 1:
+    writeFile()
+    fpOutput.close()
+    print '\nInput file has missing 9th and/or 10th column. New file: %s' % outputFile
+else:
+    print '\nInput file does not have missing 9th or 10th column'
+fpInput.close()
---
+++ bin/addColumns.sh
@@ -0,0 +1,96 @@
+#!/bin/sh 
+#
+#  addColumns.sh
+###########################################################################
+#
+#  Purpose:
+#
+#
+#  Usage:
+#
+#      addColumns.sh  filename 
+#
+#      where
+#          filename = path to the input file
+#
+#  Env Vars:
+#
+#      See the configuration file
+#
+#  Inputs:
+#
+#      - tab delimitted file
+#
+#  Outputs:
+#
+#      - Log file (${ADD_COLUMNS_LOGFILE})
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal error occurred
+#
+#  Assumes:  Nothing
+#
+#  Implementation:
+#
+#  Notes:  None
+#
+###########################################################################
+
+CURRENTDIR=`pwd`
+BINDIR=`dirname $0`
+
+CONFIG=`cd ${BINDIR}/..; pwd`/mcvload.config
+
+USAGE='Usage: addColumns.sh  filename '
+
+#
+# Make sure an input file was passed to the script. 
+#
+if [ $# -eq 1 ]
+then
+    INPUT_FILE=$1
+else
+    echo ${USAGE}; exit 1
+fi
+
+#
+# Make sure the configuration file exists and source it.
+#
+if [ -f ${CONFIG} ]
+then
+    . ${CONFIG}
+else
+    echo "Missing configuration file: ${CONFIG}"
+    exit 1
+fi
+
+#
+# Make sure the input file exists (regular file or symbolic link).
+#
+if [ "`ls -L ${INPUT_FILE} 2>/dev/null`" = "" ]
+then
+    echo "Missing input file: ${INPUT_FILE}"
+    exit 1
+fi
+
+ADD_COLUMNS_LOGFILE=${CURRENTDIR}/`basename ${ADD_COLUMNS_LOGFILE}`
+
+echo "CURRENTDIR:         ${CURRENTDIR}"
+echo "ADD_COLUMNS_LOGFILE:   ${ADD_COLUMNS_LOGFILE}"
+#
+# Initialize the log file.
+#
+LOG=${ADD_COLUMNS_LOGFILE}
+rm -rf ${LOG}
+touch ${LOG}
+
+${MCVLOAD}/bin/addColumns.py ${INPUT_FILE} | tee -a ${ADD_COLUMNS_LOGFILE}
+if [ $? -ne 0 ]
+then
+    echo 'addColumns.sh failed' | tee -a ${ADD_COLUMNS_LOGFILE}
+    exit 1
+fi
+
+exit 0
